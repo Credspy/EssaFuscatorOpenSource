328fb10eda6dfa09ef89e09afabb8c0dc25669b9
Credspy <pleumzaza555@gmail.com>
2023-01-13 13:12:31 +0700

    Update Program.cs
    Change output file name

--- EssaFuscatorMain/EssaFuscator/CLI/Program.cs
+++ EssaFuscatorMain/EssaFuscator/CLI/Program.cs
@@ -37,26 +37,17 @@ namespace EssaFuscator.CLI
 			if (!session.Item1)
 			{
 				Console.WriteLine("ERR: " + err);
-				return; 
-			}
-			
-			File.WriteAllText("temp/out.lua", @$"--[=[
-	EssaFuscator ( Private )
-	discord.gg/essa | ! EssaPrime#0001
-	Obfuscated At: {DateTime.Now:yyyy-MM-dd h:mm:ss tt}
-	Script ID: EssaFuscator_{Obfuscation.EssaFingerprint}
---]=]
-
-{session.Item2}");
+				var fileName = Path.GetFileNameWithoutExtension(arg[0]);
+			File.WriteAllText("temp/{fileName}.lua", @$"{session.Item2}");
 			
 			if (isAntiTamper) {
 				Console.WriteLine("Anti Tamper Activating");
 				var scr = File.ReadAllText("temp/out.lua");
 				//$"EssaFuscator_{Obfuscation.EssaFingerprint}", $"{args[1]}"
-				File.WriteAllText("temp/out.lua", Obfuscator.Encryption.AdvancedAntiTamper.GetLoader(scr, charsets[charsetIndex], charsetLengths[charsetIndex]), Encoding.UTF8);
+				File.WriteAllText("temp/{fileName}.lua", Obfuscator.Encryption.AdvancedAntiTamper.GetLoader(scr, charsets[charsetIndex], charsetLengths[charsetIndex]), Encoding.UTF8);
 				return;
 			}
-			
+
 			Console.WriteLine("Done!");
 		}
 	}
